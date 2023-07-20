a4dbcb6ac936c22b2b6c91819aacd4d091d31d18
Luca Kutzner <lucakutzner@gmail.com>
2023-07-20 19:20:21 +0200

    Try Access Header

--- src/components/translate.js
+++ src/components/translate.js
@@ -3,7 +3,7 @@ import * as url from "url";
 url.URLSearchParams = URLSearchParams;
 
 async function ProcessChatText(content, targetLanguage) {
-	const translator = new deepl.Translator(process.env.REACT_APP_DEEPL_KEY);
+	const translator = new deepl.Translator(process.env.REACT_APP_DEEPL_KEY, {headers: ["Access-Control-Allow-Origin"]});
 	return await translator.translateText(content, null, targetLanguage);
 }
 export default ProcessChatText
