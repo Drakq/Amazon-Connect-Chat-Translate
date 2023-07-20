e35834a9cb80a5ca2f690bf7b1919e508fbc4907
Luca Kutzner <lucakutzner@gmail.com>
2023-07-20 20:25:54 +0200

    Update Lambda function

--- amplify/backend/function/amazonTranslateLambda/src/index.js
+++ amplify/backend/function/amazonTranslateLambda/src/index.js
@@ -1,47 +1,12 @@
-//const AWS = require('aws-sdk');
-//const translate = new AWS.Translate({ apiVersion: '2017-07-01' }); // Fix API version (best practice)
-
 const deepl = require('deepl-node');
 const translator = new deepl.Translator(process.env.DEEPL_KEY);
 
 exports.handler = (event, context, callback) => {
-  let payload = JSON.parse(event.body);
-  //console.log("event: ", event);
-  //console.log("event: ", payload.terminologyNames);
-  //body: '{"content":"hello","sourceLang":"en","targetLang":"en"}'
-
-  /*let params = {
-    SourceLanguageCode: payload.sourceLang,
-    //required
-    TargetLanguageCode: payload.targetLang,
-    //required
-    Text: payload.content,
-    //required
-    TerminologyNames: payload.terminologyNames
-  };
-  console.log("parameters: " + JSON.stringify(params));*/
-  
-  translator.translateText(payload.content, null, payload.targetLang).then((response) => {
-	console.log('response ' + JSON.stringify(response));
-	callback(null, {"statusCode": 200, headers: { "Access-Control-Allow-Origin": "*", "Access-Control-Allow-Headers": "*" }, "body": JSON.stringify((response))});
-  }).catch((error) => {
-    console.error(error);
-    callback(null, {"statusCode": 500, headers: { "Access-Control-Allow-Origin": "*", "Access-Control-Allow-Headers": "*" }, "body": JSON.stringify((error))});
-  });
-
-  /*translate.translateText(
-    params,
-    function(error, response) {
-
-      if (error) {
-        console.log(error);
-        callback(null, { "statusCode": 500, headers: { "Access-Control-Allow-Origin": "*", "Access-Control-Allow-Headers": "*" }, "body": JSON.stringify((error)) });
-      }
-      else {
-        console.log('response ' + JSON.stringify(response));
-        callback(null, { "statusCode": 200, headers: { "Access-Control-Allow-Origin": "*", "Access-Control-Allow-Headers": "*" }, "body": JSON.stringify((response)) });
-      }
-    }
-
-  );*/
+	translator.translateText(event.content, null, event.targetLanguage).then((response) => {
+		console.log('response ' + JSON.stringify(response));
+		callback(null, {"statusCode": 200, headers: {"Access-Control-Allow-Origin": "*", "Access-Control-Allow-Headers": "*"}, "body": JSON.stringify(response)});
+	}).catch((error) => {
+		console.error(error);
+		callback(null, {"statusCode": 500, headers: {"Access-Control-Allow-Origin": "*", "Access-Control-Allow-Headers": "*"}, "body": JSON.stringify(error)});
+	});
 };
