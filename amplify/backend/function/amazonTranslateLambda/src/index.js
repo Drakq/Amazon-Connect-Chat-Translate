84d46f91d3af81ab054e557f6f3389c168a7ec26
Luca Kutzner <lucakutzner@gmail.com>
2023-08-01 14:00:18 +0200

    Remove "body" encapsulation

--- amplify/backend/function/amazonTranslateLambda/src/index.js
+++ amplify/backend/function/amazonTranslateLambda/src/index.js
@@ -1,9 +1,7 @@
 const deepl = require('deepl-node');
 const translator = new deepl.Translator(process.env.DEEPL_KEY);
 
-exports.handler = (event, context, callback) => {
-	let body = JSON.parse(event.body);
-	
+exports.handler = (body, context, callback) => {
 	translator.translateText(body.content, null, body.targetLanguage).then(response => {
 		console.log('Response: ' + JSON.stringify(response));
 		callback(null, {"statusCode": 200, headers: {"Access-Control-Allow-Origin": "*", "Access-Control-Allow-Headers": "*"}, "body": JSON.stringify(response)});
